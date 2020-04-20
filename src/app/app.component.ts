af122f9abab467c80e817436feeef4a794e02deb
Angel Zlatenov <angel.zl@abv.bg>
2020-04-20 22:06:40 +0300

    sortable grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AccountManagingClient';
+export class AppComponent implements OnInit{
+  columnDefs = [
+    {headerName: 'First name', field: 'firstName'},
+    {headerName: 'Last name', field: 'lastName'},
+    {headerName: 'Email', field: 'email'},
+    {headerName: 'Birth Date', field: 'birthDate'}
+  ];
+
+  rowData = [];
+
+  ngOnInit() {
+    fetch('http://localhost:8080/api/users')
+      .then(result => result.json())
+      .then(rowData => this.rowData = rowData);
+  }
+
+  onRowClickedEvent($event: any) {
+    // this.openDialog();
+  }
 }
