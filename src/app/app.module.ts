dd8c88215393e8f75c718b0fac4e96bf28331e3d
Angel Zlatenov <angel.zl@abv.bg>
2020-04-20 22:32:32 +0300

    proper routing created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {AgGridModule} from 'ag-grid-angular';
 import { SortableGridComponent } from './sortable-grid/sortable-grid.component';
+import {RouterModule, Routes} from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path : 'users', component : SortableGridComponent},
+  {path : 'PageNotFound', component : PageNotFoundComponent},
+  {path : '**', redirectTo : '/PageNotFound'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SortableGridComponent
+    SortableGridComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AgGridModule.withComponents(null)
+    AgGridModule.withComponents(null),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
