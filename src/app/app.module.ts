d9a7b8ef3b6bda5dc02505d4a886e77a6e1a8f19
Angel Zlatenov <angel.zl@abv.bg>
2020-04-22 01:34:06 +0300

    added header and some bootstrap changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import {   MatNativeDateModule } from '@angular/material/core';
 import {MatInputModule} from '@angular/material/input';
+import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
   {path : '', component : SortableGridComponent},
@@ -30,7 +31,8 @@ const routes: Routes = [
     SortableGridComponent,
     PageNotFoundComponent,
     UserComponent,
-    AddUserComponent
+    AddUserComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
